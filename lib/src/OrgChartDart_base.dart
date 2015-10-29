9ab467d9f69eda9872a72d137abcf04dc7d96a1e
decene_a <alexandredeceneux@gmail.com>
2015-10-29 17:41:52 +0100

    Version fonctionelle - suppresion du log

--- lib/src/OrgChartDart_base.dart
+++ lib/src/OrgChartDart_base.dart
@@ -5,65 +5,41 @@
 
 library OrgChartDart.base;
 
+import 'dart:html' as html;
 import 'dart:js';
 
 class OrgChartDart {
 
-  JsObject chart;
+  JsObject  chart;
+  String    targetId;
 
-  OrgChartDart(){
-    print("New Org Chart");
-  }
+  OrgChartDart([this.targetId = "chart_div"]);
 
   void  load(){
-
-    print("Load Function");
-
     var packages = context["packages"];
     new JsObject(context["google"]["load"], ["visualization", "1", {packages:["orgchart"]}]);
-
-    print("Call closure");
-
     new JsObject(context["google"]["setOnLoadCallback"], [drawChart]);
   }
 
   void  drawChart([event]){
-
-    print("Starting data table");
-
     var data = new JsObject(context["google"]["visualization"]["DataTable"], []);
-
-    print("Starting addColumn serie");
-
     data.callMethod("addColumn", ["string", "Name"]);
     data.callMethod("addColumn", ["string", "Manager"]);
     data.callMethod("addColumn", ["string", "ToolTip"]);
-
-    print("Starting addRows serie");
-
-    data.callMethod("addRows", [
-      [
-        [{'v':'Mike', 'f':'Mike<div style="color:red; font-style:italic">President</div>'}, '', 'The President'],
-        [{'v':'Jim', 'f':'Jim<div style="color:red; font-style:italic">Vice President</div>'}, 'Mike', 'VP'],
-        ['Alice', 'Mike', ''],
-        ['Bob', 'Jim', 'Bob Sponge'],
-        ['Carol', 'Bob', '']
-      ]
+    var tab = new JsObject.jsify(
+    [
+    [{'v':'Mike', 'f':'Mike<div style="color:red; font-style:italic">President</div>'}, '', 'The President'],
+    [{'v':'Jim', 'f':'Jim<div style="color:red; font-style:italic">Vice President</div>'}, 'Mike', 'VP'],
+    ['Alice', 'Mike', ''],
+    ['Carol', 'Jim', ''],
+    ['Bob', 'Alice', 'Bob Sponge']
     ]);
-
-    print("Get div chart");
-
-    var divChart = new JsObject(context["document"]["getElementById"], ["chart_div"]);
-
-    print("Get chart obj");
-
-    chart = new JsObject(context["google"]["visualization"]["OrgChart"], [divChart]);
-
-    print("draw chart");
-
-    chart.callMethod("draw", [data, {'allowHtml':true}]);
-
-    print("ending draw function");
+    data.callMethod("addRows", [
+      tab
+    ]);
+    var divChart = new JsObject.jsify([html.querySelector("#${targetId}")]);
+    chart = new JsObject(context["google"]["visualization"]["OrgChart"], [divChart[0]]);
+    chart.callMethod("draw", [data, new JsObject.jsify({'allowHtml':true})]);
   }
 
 }
