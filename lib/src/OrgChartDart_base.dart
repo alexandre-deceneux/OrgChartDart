10f964710419f182e80259786198351ba5297d77
decene_a <alexandredeceneux@gmail.com>
2015-10-29 21:46:36 +0100

    Arbre dynamique

--- lib/src/OrgChartDart_base.dart
+++ lib/src/OrgChartDart_base.dart
@@ -11,37 +11,29 @@ import 'dart:js';
 class OrgChartDart {
 
   JsObject  chart;
-  String    targetId;
+  String    targetId = "chart_div";
+  var       tree =  [
+    [{'v':'Mike', 'f':'Mike<div style="color:red; font-style:italic">President</div>'}, '', 'The President'],
+    [{'v':'Jim', 'f':'Jim<div style="color:red; font-style:italic">Vice President</div>'}, 'Mike', 'VP'],
+    ['Alice', 'Mike', ''],
+    ['Carol', 'Jim', ''],
+    ['Bob', 'Alice', 'Bob Sponge']
+  ];
 
-  OrgChartDart([this.targetId = "chart_div"]){
-    print("ORGACHARTDART: new chart");
-  }
+  OrgChartDart([this.targetId, this.tree]);
 
   void  load(){
-    print("ORGACHARTDART: load chart");
     var packages = context["packages"];
     new JsObject(context["google"]["load"], ["visualization", "1", {packages:["orgchart"]}]);
     new JsObject(context["google"]["setOnLoadCallback"], [drawChart]);
   }
 
   void  drawChart([event]){
-    print("ORGACHARTDART: Draw chart");
-
     var data = new JsObject(context["google"]["visualization"]["DataTable"], []);
     data.callMethod("addColumn", ["string", "Name"]);
     data.callMethod("addColumn", ["string", "Manager"]);
     data.callMethod("addColumn", ["string", "ToolTip"]);
-    var tab = new JsObject.jsify(
-    [
-    [{'v':'Mike', 'f':'Mike<div style="color:red; font-style:italic">President</div>'}, '', 'The President'],
-    [{'v':'Jim', 'f':'Jim<div style="color:red; font-style:italic">Vice President</div>'}, 'Mike', 'VP'],
-    ['Alice', 'Mike', ''],
-    ['Carol', 'Jim', ''],
-    ['Bob', 'Alice', 'Bob Sponge']
-    ]);
-    data.callMethod("addRows", [
-      tab
-    ]);
+    data.callMethod("addRows", [new JsObject.jsify(tree)]);
     var divChart = new JsObject.jsify([html.querySelector("#${targetId}")]);
     chart = new JsObject(context["google"]["visualization"]["OrgChart"], [divChart[0]]);
     chart.callMethod("draw", [data, new JsObject.jsify({'allowHtml':true})]);
